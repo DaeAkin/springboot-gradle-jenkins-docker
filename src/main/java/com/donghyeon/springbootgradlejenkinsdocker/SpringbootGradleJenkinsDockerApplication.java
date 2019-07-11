13e4f4ab06da26ac0a03f60283620de414a4416c
민동현 <mindonghyeon890@gmail.com>
2019-07-11 14:47:33 +0900

    initial commit

---
+++ src/main/java/com/donghyeon/springbootgradlejenkinsdocker/SpringbootGradleJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.donghyeon.springbootgradlejenkinsdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradleJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGradleJenkinsDockerApplication.class, args);
+	}
+
+}
